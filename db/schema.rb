e4da0100ca3f43a204c8fcab0df5e460bf09db83
brandoshmando <brancraft@gmail.com>
2014-12-15 22:59:41 -0500

    Add db/shchema to git ignore even though it doesnt seem to be working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216034615) do
+ActiveRecord::Schema.define(version: 20141216035440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 20141216034615) do
 
   create_table "sections", force: true do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "students", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
