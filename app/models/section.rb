aacfbf09d566610c00c03cf9e3d2d2ff703eccc2
brandoshmando <brancraft@gmail.com>
2014-12-29 22:43:52 -0500

    Add relationships to section model and migration with course_id for belongs to relationship

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,2 +1,6 @@
 class Section < ActiveRecord::Base
+  has_one :roster
+  has_many :grades
+  has_and_belongs_to_many :assistants
+  belongs_to :course
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216041128) do
+ActiveRecord::Schema.define(version: 20141230034101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20141216041128) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "course_id"
   end
 
   create_table "students", force: true do |t|
