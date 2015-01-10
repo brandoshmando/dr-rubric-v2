e6dece0e4f4344efbd6101fe585ab6c9f88778b3
brandoshmando <brancraft@gmail.com>
2015-01-10 17:35:56 -0500

    Successful test of saves of first/last name

--- spec/v1/models/professor_spec.rb
+++ spec/v1/models/professor_spec.rb
@@ -11,12 +11,37 @@ describe Professor do
   it { should validate_presence_of :type }
 
   describe "model validates" do
-    context "first_name is less than fifty charcters" do
+    context "first_name is greater than fifty charcters" do
       it "is not saved successfully" do
         name = "a" * 51
         user = build(:professor, first_name: name)
         expect(user).not_to be_valid
       end
     end
+
+    context "first_name is less than fifty charcters" do
+      it "is saved successfully" do
+        name = "a" * 32
+        user = build(:professor, first_name: name)
+        expect(user).to be_valid
+      end
+    end
+
+
+    context "last_name is greater than fifty charcters" do
+      it "is not saved successfully" do
+        name = "a" * 51
+        user = build(:professor, last_name: name)
+        expect(user).not_to be_valid
+      end
+    end
   end
+
+  context "last_name is less than fifty charcters" do
+      it "is saved successfully" do
+        name = "a" * 32
+        user = build(:professor, first_name: name)
+        expect(user).to be_valid
+      end
+    end
 end
