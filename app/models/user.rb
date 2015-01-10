54a4858fd936479c748f9653ad94795007dcb437
brandoshmando <brancraft@gmail.com>
2015-01-10 17:59:49 -0500

    Test validations for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   validates :first_name, presence: :true, length: { maximum: 50 }
   validates :last_name, presence: :true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true, length: { maximum: 100 }
+  validates :email, presence: true,  uniqueness: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
 end
--- spec/v1/models/professor_spec.rb
+++ spec/v1/models/professor_spec.rb
@@ -44,4 +44,38 @@ describe Professor do
       expect(user).to be_valid
     end
   end
+
+  context "email is in valid email format" do
+
+    it "is invalid with invalid email format" do
+      email = "blahblah"
+      user = build(:professor, email: email)
+      expect(user).not_to be_valid
+    end
+
+    it "is valid with valid email format" do
+      email = "blahblah@example.com"
+      user = build(:professor, email: email)
+      expect(user).to be_valid
+    end
+
+    it "is invalid when email exceed max length" do
+      a_string = "a" * 26
+      email =  a_string + "@" + a_string
+      user = build(:professor, email: email)
+    end
+
+    it "is invalid when user with email already exists" do
+      user1 = create(:professor, email:"brando@example.com")
+      user2 = build(:professor, email:"brando@example.com")
+
+      expect(user1).to be_valid
+      expect(user2).not_to be_valid
+    end
+  end
 end
+
+
+
+
+
