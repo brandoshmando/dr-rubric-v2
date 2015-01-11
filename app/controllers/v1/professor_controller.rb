84c424f124d59ff67d1c2a73f98fc4e59769610d
brandoshmando <brancraft@gmail.com>
2015-01-10 23:18:13 -0500

    Build out skeleton of prof controller. Need to play with active model serializers to continue

---
+++ app/controllers/v1/professor_controller.rb
@@ -0,0 +1,25 @@
+class V1::ProfessorController < ApplicationController
+
+  def create
+    @professor = Professor.new(params[:professor])
+
+    if professor.save
+      #serialize success response
+    else
+      #serialize errors
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+private
+  def professor_params
+    params.require(:professor).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end
