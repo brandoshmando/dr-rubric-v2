0a47286780bd5e9f8224680c4a99707932630acf
brandoshmando <brancraft@gmail.com>
2015-01-10 21:43:40 -0500

    Add password digest column to users table and migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_secure_password
+
   validates :first_name, presence: :true, length: { maximum: 50 }
   validates :last_name, presence: :true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141230043644) do
+ActiveRecord::Schema.define(version: 20150111024101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 20141230043644) do
     t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
