07d37cc2fecb6a3fe6aa0db2fce94afd07a1906e
brandoshmando <brancraft@gmail.com>
2015-01-24 15:11:49 -0500

    Assistants relationships tested

--- app/models/assistant.rb
+++ app/models/assistant.rb
@@ -1,7 +1,7 @@
 class Assistant < User
   #Relationships
   has_many :grades, as: :gradeable
-  has_and_belongs_to_many :sections
+  has_and_belongs_to_many :sections, join_table: :assistants_sections
 
   #Validations
   validates :type, presence: :true
