8bc0b5365115a567a2849f75a3c5d99c2d65011e
brandoshmando <brancraft@gmail.com>
2015-01-24 16:34:52 -0500

    Relationship/validations for criteria done

--- spec/models/criteria_spec.rb
+++ spec/models/criteria_spec.rb
@@ -8,4 +8,5 @@ describe Criteria, :type => :model do
   it { should validate_presence_of :body }
   it { should ensure_length_of(:body).is_at_most(500) }
   it { should have_many :comments }
+  it { should belong_to :eval }
 end
