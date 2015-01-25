862c260e9e7eccee9d0e0ae1cd181a1d25a97334
brandoshmando <brancraft@gmail.com>
2015-01-24 21:02:38 -0500

    Comment tests completed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   has_many :comment_scores, as: :comment_scoreable
-  belongs_to :criterion
+  belongs_to :criteria
+
+  #Validations
+  validates :body, presence: :true, length: { maximum: 500 }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,4 +4,10 @@ describe Comment, :type => :model do
   it "has valid factory" do
     expect(create(:comment)).to be_valid
   end
+
+  it { should validate_presence_of :body }
+  it { should ensure_length_of(:body).is_at_most(500) }
+  it { should have_many :comment_scores }
+  it { should belong_to :criteria }
+
 end
