4c6c4fcce180a8787d5849ae0fd485aa711c2a0b
brandoshmando <brancraft@gmail.com>
2015-01-24 21:15:41 -0500

    CommentScore tests complete

--- spec/models/comment_score_spec.rb
+++ spec/models/comment_score_spec.rb
@@ -4,4 +4,8 @@ describe CommentScore, :type => :model do
   it "has valid factory" do
     expect(create(:comment_score)).to be_valid
   end
+
+  it { should validate_presence_of :body }
+  it { should ensure_length_of(:body).is_at_most(500) }
+  it { should belong_to :comment_scoreable }
 end
