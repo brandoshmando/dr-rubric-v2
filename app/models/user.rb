770c24d6776e83362fac3bc8bc367ca1410e315b
brandoshmando <brancraft@gmail.com>
2015-02-22 19:58:18 -0500

    Route's setup. Other formatting I wanted

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable
+
   validates :first_name, presence: :true, length: { maximum: 50 }
   validates :last_name, presence: :true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {sessions:'sessions'}, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout' }
+  #Custom Devise paths
+  #Sessions
+  post '/users/login/', to: 'sessions#create', as: :create_user_session
+  delete '/users/logout/', to: 'sessions#destroy', as: :destroy_user_session
 
+  #Professors
+  post '/professors/', to: 'devise/registrations#create'
+  post '/professors/password/', to: 'devise/passwords#create', as: :professor_password
+  patch '/professors/password/', to: 'devise/passwords#update'
+  put '/professors/password/', to: 'devise/passwords#update'
+  post '/professors/unlock/', to: 'devise/unlocks#create', as: :professor_unlock
+
+  #Assistants
+  post '/assistants/', to: 'devise/registrations#create'
+  post '/assistants/password/', to: 'devise/passwords#create', as: :assistant_password
+  patch '/assistants/password/', to: 'devise/passwords#update'
+  put '/assistants/password/', to: 'devise/passwords#update'
+  post '/assistants/unlock/', to: 'devise/unlocks#create', as: :assistant_unlock
+
+  #Eventually when I want to make an api for the platform
   namespace :v1, defaults: { format: :json } do
   end
-
-  post '/professors/', to: 'professor#create'
 end
