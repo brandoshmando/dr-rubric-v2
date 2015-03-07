326ed645caba5bfd40d7f47e89840cc6d880c545
brandoshmando <brancraft@gmail.com>
2015-03-06 23:25:46 -0500

    Tokens in. removed ghost migrations via postico

--- db/migrate/20150307023957_create_tokens.rb
+++ db/migrate/20150307023957_create_tokens.rb
@@ -1,9 +1,9 @@
 class CreateTokens < ActiveRecord::Migration
   def change
     create_table :tokens do |t|
-      t.string :value
-      t.datetime :expires_at
-      t.integer :user_id, unique: :true
+      t.string :value, null: false
+      t.datetime :expires_at, null: false
+      t.integer :user_id, null: false
       t.timestamps
     end
   add_index(:tokens, :user_id, unique: :true)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130020404) do
+ActiveRecord::Schema.define(version: 20150307023957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -135,36 +135,25 @@ ActiveRecord::Schema.define(version: 20150130020404) do
     t.datetime "updated_at"
   end
 
+  create_table "tokens", force: true do |t|
+    t.string   "value",      null: false
+    t.datetime "expires_at", null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tokens", ["user_id"], name: "index_tokens_on_user_id", unique: true, using: :btree
+
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email",                               null: false
+    t.string   "email",      null: false
     t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.string   "authentication_token"
   end
 
-  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
 end
